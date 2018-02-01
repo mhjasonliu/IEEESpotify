9b313bc3569edf6521b46124dfe750878c05679b
mhjasonliu <mhjasonliu@gmail.com>
2018-01-31 19:30:02 -0600

    database code moved

--- authorization_code/app.js
+++ authorization_code/app.js
@@ -152,115 +152,16 @@ app.get('/refresh_token', function(req, res) {
 var hasher = require("./hasher");
 var ie3database = require("./db");
 
-mongoose.connect(ie3database.uri);
+mongoose.connect(ie3database.uri); //connect to database
+ie3database.setup(app); //set up database funcitonality
 
 var db = mongoose.connection;
-
 db.on('error', console.error.bind(console,'connection error:'));
 db.once('open', function() {
     console.log('database connection successful ');
 });
 
-var DBEntry = ie3database.DBEntry;
 
-app.get('/dblogin', function(req,res){
-    var userlogin = req.query.username;
-    var userID = req.query.userID;
-
-    console.log("received userlogin info: " + userlogin);
-    DBEntry.find({username: userlogin, userID: userID}, function(err, doc) {
-        if(err) return console.log(err);
-        if(doc.length == 0) {
-            console.log("user does not currently exist. attempting to create");
-            var newEntry = ie3database.addNewEntry(userlogin,userID);
-            res.send({
-                'trackList' : newEntry.listOfTracks
-            });
-        }
-        else {
-            console.log("user found");
-            res.send({
-                'trackList' : doc[0].listOfTracks
-            });
-        }
-    });
-
-
-});
-
-app.post('/add_new_track', function(req,res){
-
-    var newTrack = req.body.newTrack;
-    var userlogin = req.body.username;
-    var userID = req.body.userID;
-    var access_token = req.body.access_token;
-
-    console.log("Attempting to add track");
-
-        DBEntry.findOne({username:userlogin, userID: userID},function(err, doc){
-            if(err) return console.log(err);
-
-            if(doc === null) {
-                res.send({
-                    'err': ["You must be logged in to access."]
-                });
-                return;
-            }
-
-            var trackObj = {};
-            trackObj.uri = newTrack;
-
-            if(doc.listOfTracks.find(function(x){return x.track.uri == newTrack})=== undefined)
-                doc.listOfTracks.push({track: trackObj, listOfStrings: []});
-
-            doc.save(function(err){
-                if(err) return console.log(err);
-                console.log("update successful");
-            });
-            res.send({
-                'trackList': doc.listOfTracks
-            });
-        });
-
-});
-
-app.post('/associate_word',function(req,res){
-    var userlogin = req.body.username,
-        userID = req.body.userID,
-        trackuri = req.body.current_track_uri,
-        new_word = req.body.new_word,
-        access_token = req.body.access_token;
-
-    DBEntry.findOne({username:userlogin, userID: userID},function(err, doc){
-        if(err) return console.log(err);
-
-        if(doc === null) {
-            res.send({
-                'err': "You must be logged in to access."
-            });
-            return;
-        }
-
-        var idx = doc.listOfTracks.findIndex(function(element) {
-            //console.log(element.track.uri + " " + trackuri);
-            return element.track.uri == trackuri});
-        if(idx != -1)
-            doc.listOfTracks[idx].listOfStrings.push(new_word);
-        else
-            res.send({err: "track not found in database."});
-
-        doc.save(function(err){
-            if(err)
-                return console.log(err);
-            else
-                console.log("update successful");
-        });
-        res.send({
-            'listOfStrings': doc.listOfTracks[idx].listOfStrings
-        });
-    });
-
-});
 
 console.log('Listening on 8888');
 app.listen(8888);
