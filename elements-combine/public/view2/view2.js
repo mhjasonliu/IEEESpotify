7c0efd3be505896905a3990148e952553767fad7
mhjasonliu <mhjasonliu@gmail.com>
2018-02-04 20:52:35 -0600

    back-end and front-end successfully connected

--- elements-combine/public/view2/view2.js
+++ elements-combine/public/view2/view2.js
@@ -7,4 +7,21 @@ angular.module('frontApp.view2', ['ngRoute'])
 
     .controller('View2Ctrl', ['$scope', function($scope) {
         $scope.personName = "tommy";
+        var param = getHashParams();
+        $scope.access_token = param.access_token;
+        $scope.refresh_token = param.refresh_token;
+
+        function getHashParams() {
+            var hashParams = {};
+            const urllen = "#!/view2#".length;
+            var e, r = /([^&;=]+)=?([^&;]*)/g,
+                q = window.location.hash.substring(urllen);
+
+            while ( e = r.exec(q)) {
+                hashParams[e[1]] = decodeURIComponent(e[2]);
+            }
+            return hashParams;
+        }
+
+
     }]);
